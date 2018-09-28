02ba294fad41bdc219404eef2e6b0731a07ba0fc
hawkingSao <shingo.kawashima@one-star.jp>
2018-09-28 22:58:33 +0900

    シンボルを用いてリファクタリング

--- no001/problem.rb
+++ no001/problem.rb
@@ -5,5 +5,4 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 =end
 
-element = [*1...1000].select { |n| n % 3 == 0 || n % 5 == 0 }
-puts element.inject { |result, n| result + n }
+puts [*1...1000].select { |n| n % 3 == 0 || n % 5 == 0 }.inject(:+)
