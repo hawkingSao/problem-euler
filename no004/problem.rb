db7deef5cfbf34edb9646a1ca4ad6611e8c8eba5
hawkingSao <shingo.kawashima@one-star.jp>
2018-10-02 14:04:06 +0900

    ハッシュ記法の微修正

--- no004/problem.rb
+++ no004/problem.rb
@@ -9,7 +9,7 @@ def palindrome?(result)
   result.to_s.reverse == result.to_s
 end
 
-answer = {palindromic_number: 0, n: 100, i: 100}
+answer = { palindromic_number: 0, n: 100, i: 100 }
 999.downto(100) do |n|
   999.downto(n) do |i|
     result = n * i
