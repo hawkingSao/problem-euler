bd3b5691388f5d2c8bcfab7eab8b8d430a15cfac
hawkingSao <shingo.kawashima@one-star.jp>
2018-10-04 00:31:16 +0900

    問5:lcmメソッドで簡素化

--- no005/problem.rb
+++ no005/problem.rb
@@ -5,16 +5,4 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 =end
 
-require 'prime'
-prime_factors_array = Prime.each(20).to_a
-
-prime_factors_index_array = prime_factors_array.map do |n|
-  index = 0
-  loop do
-    index += 1
-    break if 20 < n ** (index + 1)
-  end
-  n ** index
-end
-
-puts prime_factors_index_array.inject(:*)
+puts [*1..20].inject(:lcm)
