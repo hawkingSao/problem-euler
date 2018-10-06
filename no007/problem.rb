5b506b777c9f11b5bd6dbee2fac39c349dce172e
hawkingSao <shingo.kawashima@one-star.jp>
2018-10-06 16:10:56 +0900

    問7：10001個目の素数

---
+++ no007/problem.rb
@@ -0,0 +1,9 @@
+=begin
+問題文
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10001st prime number?
+=end
+
+require 'prime'
+puts Prime.take(10001).last
