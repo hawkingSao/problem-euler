c5830ad29bab13983c3252fbe93df513900e7f3d
hawkingSao <kawashingo1990@gmail.com>
2018-11-14 13:16:22 +0900

    3を渡すとthreeで返ってきてほしいがoneで返ってきて落ちるテスト

--- no017/test_problem.rb
+++ no017/test_problem.rb
@@ -8,5 +8,7 @@ class ProblemTest < Minitest::Test
   def test_problem
     assert_equal 'one', 1.to_l
     assert_equal 3, 1.to_l.size
+    assert_equal 'three', 3.to_l
+    assert_equal 5, 3.to_l.size
   end
 end
