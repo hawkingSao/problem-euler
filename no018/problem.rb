3d811f41105e9666c69ace1537548bc94cfd084c
hawkingSao <kawashingo1990@gmail.com>
2018-11-15 18:33:04 +0900

    三角形状の数字を底辺列から削りながら、最大値を計算

--- no018/problem.rb
+++ no018/problem.rb
@@ -3,3 +3,18 @@ def path_array
     numbers.each_line.map { |line| line.split.map(&:to_i) }.reverse
   end
 end
+
+total = path_array
+bottom = total.shift
+
+until total.empty?
+  new_bottom = []
+  total.first.each_with_index do |number, i|
+    bigger = bottom[i] > bottom[i + 1] ? bottom[i] : bottom[i + 1]
+    new_bottom << number + bigger
+  end
+  bottom = new_bottom
+  total.shift
+end
+
+puts bottom.max
