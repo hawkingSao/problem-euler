09b419f63e8ea8599803068f865a855fbe964874
hawkingSao <kawashingo1990@gmail.com>
2018-11-16 12:22:22 +0900

    wdayメソッドを用いてテストを通す

--- no019/problem.rb
+++ no019/problem.rb
@@ -1,5 +1,5 @@
 require 'date'
 
 def count_sunday(start_year, start_month, end_year, end_month)
-  Date.new(start_year, start_month).upto(Date.new(end_year, end_month)).select { |day| day.mday == 1 }.count
+  Date.new(start_year, start_month).upto(Date.new(end_year, end_month)).select { |day| day.mday == 1 && day.wday.zero? }.count
 end
