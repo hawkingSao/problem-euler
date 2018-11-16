c479b836deffe6ff29906a03053246b79d9bc224
hawkingSao <kawashingo1990@gmail.com>
2018-11-16 12:18:54 +0900

    毎月1日だけをカウントするようにしてテストを通す

--- no019/problem.rb
+++ no019/problem.rb
@@ -1,5 +1,5 @@
 require 'date'
 
 def count_sunday(start_year, start_month, end_year, end_month)
-  Date.new(start_year, start_month).upto(Date.new(end_year, end_month)).count
+  Date.new(start_year, start_month).upto(Date.new(end_year, end_month)).select { |day| day.mday == 1 }.count
 end
