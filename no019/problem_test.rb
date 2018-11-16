7b934cc5c13ed32a7cc22198a1704ee038396e7f
hawkingSao <kawashingo1990@gmail.com>
2018-11-16 12:18:18 +0900

    日数ではなく月数をカウントしたいのでテストを変更

--- no019/problem_test.rb
+++ no019/problem_test.rb
@@ -7,6 +7,6 @@ Minitest::Reporters.use!
 class ProblemTest < Minitest::Test
   def test_path_array
     assert_equal 1, count_sunday(1900, 1, 1900, 1)
-    assert_equal 335, count_sunday(1900, 1, 1900, 12)
+    assert_equal 12, count_sunday(1900, 1, 1900, 12)
   end
 end
