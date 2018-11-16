7015414011957eacece8f04bd8edd571ab467ac6
hawkingSao <kawashingo1990@gmail.com>
2018-11-16 17:13:19 +0900

    テストメソッドがコピー元だったので修正

--- no019/problem_test.rb
+++ no019/problem_test.rb
@@ -5,7 +5,7 @@ require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ProblemTest < Minitest::Test
-  def test_path_array
+  def test_count_sunday
     assert_equal 0, count_sunday(1900, 1, 1900, 1)
     assert_equal 2, count_sunday(2018, 1, 2018, 12)
   end
