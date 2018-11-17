5c75df17b0e3efa8213149a800775422c99a3917
hawkingSao <kawashingo1990@gmail.com>
2018-11-17 15:41:37 +0900

    テストを拡張してメソッドが正しいことを確認

--- no020/problem_test.rb
+++ no020/problem_test.rb
@@ -7,5 +7,7 @@ Minitest::Reporters.use!
 class ProblemTest < Minitest::Test
   def test_factorial_digit_sum
     assert_equal 3628800, factorial_digit_sum(10)
+    assert_equal 6, factorial_digit_sum(3)
+    assert_equal 120, factorial_digit_sum(5)
   end
 end
