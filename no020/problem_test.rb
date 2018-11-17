67e9113e3006e54057d4fb05865f7585828f472f
hawkingSao <kawashingo1990@gmail.com>
2018-11-17 15:42:48 +0900

    階乗の計算結果の各位の和が期待されるようにテストを変更

--- no020/problem_test.rb
+++ no020/problem_test.rb
@@ -6,8 +6,8 @@ Minitest::Reporters.use!
 
 class ProblemTest < Minitest::Test
   def test_factorial_digit_sum
-    assert_equal 3628800, factorial_digit_sum(10)
+    assert_equal 27, factorial_digit_sum(10)
     assert_equal 6, factorial_digit_sum(3)
-    assert_equal 120, factorial_digit_sum(5)
+    assert_equal 3, factorial_digit_sum(5)
   end
 end
