ffab02899999cd4706b412e9f0095eb26dcabcaf
hawkingSao <kawashingo1990@gmail.com>
2018-11-21 11:34:31 +0900

    引数が整数ではない場合は例外処理するように実装してテストを通す

--- no021/problem_test.rb
+++ no021/problem_test.rb
@@ -9,4 +9,16 @@ class ProblemTest < Minitest::Test
     assert_equal 284, d(220)
     assert_equal 220, d(284)
   end
+
+  def test_invalid
+    e = assert_raises RuntimeError do
+      d(6.5)
+    end
+    assert_equal '引数が整数ではない', e.message
+
+    e = assert_raises RuntimeError do
+      d('テスト')
+    end
+    assert_equal '引数が整数ではない', e.message
+  end
 end
