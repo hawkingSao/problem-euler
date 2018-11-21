a34ad445ccd977ab5a9907dbf9e4dea2386dbc1a
hawkingSao <kawashingo1990@gmail.com>
2018-11-21 11:37:25 +0900

    rubocopによる修正（Add empty line after guard clause.）

--- no021/problem.rb
+++ no021/problem.rb
@@ -2,6 +2,7 @@ require 'prime'
 
 def d(number)
   raise '引数が整数ではない' unless number.is_a?(Integer)
+
   geometric_progression_of_divisor = Prime.prime_division(number).map do |n, i|
     (n**(i + 1) - 1) / (n - 1)
   end
