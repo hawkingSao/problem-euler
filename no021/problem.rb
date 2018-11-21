ae6c69712a59ea0d4696891cfe104ed87dca58c8
hawkingSao <kawashingo1990@gmail.com>
2018-11-21 14:57:36 +0900

    dメソッドの引数が1になるときにZeroDivisionErrorになるので分岐

--- no021/problem.rb
+++ no021/problem.rb
@@ -3,10 +3,14 @@ require 'prime'
 def d(number)
   raise '引数が整数ではない' unless number.is_a?(Integer)
 
-  geometric_progression_of_divisor = Prime.prime_division(number).map do |n, i|
-    (n**(i + 1) - 1) / (n - 1)
+  if number != 1
+    geometric_progression_of_divisor = Prime.prime_division(number).map do |n, i|
+      (n**(i + 1) - 1) / (n - 1)
+    end
+    geometric_progression_of_divisor.inject(&:*) - number
+  else
+    0
   end
-  geometric_progression_of_divisor.inject(&:*) - number
 end
 
 def amicable?(number, comparison)
--- no021/problem_test.rb
+++ no021/problem_test.rb
@@ -8,9 +8,10 @@ class ProblemTest < Minitest::Test
   def test_sum_of_proper_divisors
     assert_equal 284, d(220)
     assert_equal 220, d(284)
+    assert_equal 0, d(1)
   end
 
-  def test_augument_class_is_not_integer
+  def test_augment_class_is_not_integer
     e = assert_raises RuntimeError do
       d(6.5)
     end
