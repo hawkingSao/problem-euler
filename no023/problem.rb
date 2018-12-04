1788a1e4a91bf409b110a2002a263094d5474644
hawkingSao <kawashingo1990@gmail.com>
2018-12-04 09:29:33 +0900

    メソッドを定義してテストを通す

--- no023/problem.rb
+++ no023/problem.rb
@@ -16,3 +16,19 @@ class Integer
     sum_of_proper_divisors > self
   end
 end
+
+def answer(from, to)
+  abundants = (from..to).select(&:abundant?)
+
+  sum_of_two_abundants = []
+  abundants.each_with_index do |n, i|
+    abundants[i..abundants.length].each do |other|
+      sum_of_two_abundants << n + other
+    end
+  end
+
+  sum_of_two_abundants.uniq!
+
+  (from..to).reject { |n| sum_of_two_abundants.include?(n) }.inject(:+)
+end
+puts answer(1, 28_123)
