9d91c97eea7108a8c1f25a46d4a96ff672571229
hawkingSao <kawashingo1990@gmail.com>
2018-12-05 09:22:35 +0900

    問2で作ったフィボナッチ数列の型を利用してテストを通す

---
+++ no025/problem.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+def nth_digit_fibonacci_number(nth_digit)
+  fibonacci_sequence = [1, 1]
+
+  loop do
+    n = fibonacci_sequence[-2, 2].inject(:+)
+    break if n.to_s.length == nth_digit
+    fibonacci_sequence << n
+  end
+
+  fibonacci_sequence.size + 1
+end
