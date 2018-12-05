c3bdcff3ad252f5cc65e5a9b7682a505015438a6
hawkingSao <kawashingo1990@gmail.com>
2018-12-05 09:23:20 +0900

    回答して終了

--- no025/problem.rb
+++ no025/problem.rb
@@ -6,8 +6,11 @@ def nth_digit_fibonacci_number(nth_digit)
   loop do
     n = fibonacci_sequence[-2, 2].inject(:+)
     break if n.to_s.length == nth_digit
+
     fibonacci_sequence << n
   end
 
   fibonacci_sequence.size + 1
 end
+
+puts nth_digit_fibonacci_number(1_000)
